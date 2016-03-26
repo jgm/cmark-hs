bdb599bc2cffe49fd7e98e94d10e1266eb908cd4
John MacFarlane <jgm@berkeley.edu>
2016-03-26 13:29:08 -0700

    Updated C sources.

--- cbits/cmark_version.h
+++ cbits/cmark_version.h
@@ -1,7 +1,7 @@
 #ifndef CMARK_VERSION_H
 #define CMARK_VERSION_H
 
-#define CMARK_VERSION ((0 << 16) | (23 << 8)  | 0)
-#define CMARK_VERSION_STRING "0.23.0"
+#define CMARK_VERSION ((0 << 16) | (25 << 8)  | 2)
+#define CMARK_VERSION_STRING "0.25.2"
 
 #endif
